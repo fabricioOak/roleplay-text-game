d918967b13ccd416f7a13d07e8cf93fe8472c798
Fabricio Carvalho <fcarvalho1999@gmail.com>
2023-12-30 00:26:03 -0300

    Add more monsters

---
+++ gameProperties/monsters.js
@@ -0,0 +1,178 @@
+export default [
+	{
+		name: "Slime",
+		level: 2,
+		health: 15,
+		description:
+			"A basic and amorphous creature that oozes and splits when attacked.",
+		abilities: ["Split", "Acidic Touch"],
+	},
+	{
+		name: "Shadowling",
+		level: 3,
+		health: 25,
+		description:
+			"A small and elusive creature that hides in the shadows, striking from darkness.",
+		abilities: ["Shadow Step", "Silent Strike"],
+	},
+	{
+		name: "Ember Sprite",
+		level: 4,
+		health: 30,
+		description:
+			"A mischievous spirit that embodies flames, the Ember Sprite can create small fire illusions.",
+		abilities: ["Fiery Illusion", "Ember Burst"],
+	},
+	{
+		name: "Stone Golem",
+		level: 5,
+		health: 35,
+		description:
+			"A resilient creature made of solid rock, the Stone Golem can withstand heavy blows.",
+		abilities: ["Rock Shield", "Earth Slam"],
+	},
+	{
+		name: "Nightmare Steed",
+		level: 6,
+		health: 45,
+		description:
+			"A ghostly horse from the underworld, the Nightmare Steed can trample enemies with otherworldly speed.",
+		abilities: ["Spectral Charge", "Ethereal Presence"],
+	},
+	{
+		name: "Giant Spider",
+		level: 7,
+		health: 50,
+		description:
+			"A venomous spider with a paralyzing bite, the Giant Spider can weave webs to trap its prey.",
+		abilities: ["Web Snare", "Venomous Bite"],
+	},
+	{
+		name: "Fanged Beast",
+		level: 8,
+		health: 60,
+		description:
+			"A swift and agile creature with sharp fangs, capable of tearing through armor.",
+		abilities: ["Pounce", "Razor Bite"],
+	},
+	{
+		name: "Swamp Hag",
+		level: 9,
+		health: 55,
+		description:
+			"A malevolent witch dwelling in swamps, the Swamp Hag can control nature and curse its foes.",
+		abilities: ["Bog Curse", "Toxic Mist"],
+	},
+	{
+		name: "Ice Golem",
+		level: 10,
+		health: 100,
+		description:
+			"A massive humanoid made of enchanted ice, the Ice Golem can freeze its enemies in their tracks.",
+		abilities: ["Frost Nova", "Icy Slam"],
+	},
+	{
+		name: "Shapeshifter",
+		level: 11,
+		health: 70,
+		description:
+			"A cunning creature capable of assuming the form of its prey, making it difficult to discern friend from foe.",
+		abilities: ["Mimicry", "Deceptive Strike"],
+	},
+	{
+		name: "Fire Elemental",
+		level: 12,
+		health: 80,
+		description:
+			"A living embodiment of flames, the Fire Elemental engulfs its foes in scorching heat. Be cautious, for it can manipulate fire to its advantage.",
+		abilities: ["Inferno Burst", "Flame Shield"],
+	},
+	{
+		name: "Storm Harpy",
+		level: 13,
+		health: 90,
+		description:
+			"A winged creature with the ability to control storms, the Storm Harpy can summon lightning and gusts of wind.",
+		abilities: ["Thunderstorm", "Wind Slash"],
+	},
+	{
+		name: "Thunder Golem",
+		level: 14,
+		health: 110,
+		description:
+			"A golem infused with the power of thunder, capable of unleashing electrifying attacks.",
+		abilities: ["Thunder Strike", "Electric Shield"],
+	},
+	{
+		name: "Spectral Wraith",
+		level: 15,
+		health: 120,
+		description:
+			"An ethereal being that can phase through walls and drain the life force from its victims.",
+		abilities: ["Phantom Phase", "Life Drain"],
+	},
+	{
+		name: "Earth Elemental",
+		level: 16,
+		health: 130,
+		description:
+			"A massive creature made of rock and earth, the Earth Elemental can cause earthquakes and create barriers.",
+		abilities: ["Quake Slam", "Stone Shield"],
+	},
+	{
+		name: "Mindflayer",
+		level: 17,
+		health: 140,
+		description:
+			"An aberrant creature with psychic abilities, the Mindflayer can manipulate minds and devour thoughts.",
+		abilities: ["Mind Blast", "Psychic Drain"],
+	},
+	{
+		name: "Gorgon Serpent",
+		level: 18,
+		health: 150,
+		description:
+			"A serpent with a petrifying gaze, the Gorgon Serpent can turn its victims into stone with a single glare. Approach with extreme caution.",
+		abilities: ["Petrifying Gaze", "Venomous Bite"],
+	},
+	{
+		name: "Lava Elemental",
+		level: 19,
+		health: 180,
+		description:
+			"A molten creature born from the heart of a volcano, the Lava Elemental can unleash rivers of magma.",
+		abilities: ["Magma Surge", "Fire Immunity"],
+	},
+	{
+		name: "Dragon",
+		level: 20,
+		health: 300,
+		description:
+			"A formidable and ancient dragon with powerful breath attacks and thick scales.",
+		abilities: ["Dragon Breath", "Scales of Resistance"],
+	},
+	{
+		name: "Celestial Guardian",
+		level: 23,
+		health: 380,
+		description:
+			"A divine being sent to protect sacred realms, the Celestial Guardian wields holy powers and celestial weapons.",
+		abilities: ["Divine Smite", "Heavenly Shield"],
+	},
+	{
+		name: "Demonic Warlord",
+		level: 25,
+		health: 400,
+		description:
+			"A fearsome warlord from the depths of the underworld, wielding dark magic and a massive demonic blade.",
+		abilities: ["Dark Slash", "Infernal Aura"],
+	},
+	{
+		name: "Shadowsoul Reaper",
+		level: 27,
+		health: 450,
+		description:
+			"A nightmarish entity that harvests souls, the Shadowsoul Reaper can phase between dimensions and unleash dark energy.",
+		abilities: ["Soul Harvest", "Dimensional Shift"],
+	},
+];
