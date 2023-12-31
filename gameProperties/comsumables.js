5a3227bc8f4784f2c79f1015e35a3b3526d89fc3
Fabricio Carvalho <fcarvalho1999@gmail.com>
2023-12-31 16:26:40 -0300

    Add comsumable potions

---
+++ gameProperties/comsumables.js
@@ -0,0 +1,26 @@
+export default [
+	{
+		name: "Minor potion",
+		cost: 20,
+		heal: 15,
+		description: "A small vial of healing liquid. It restores 15 health.",
+	},
+	{
+		name: "Standard potion",
+		cost: 50,
+		heal: 35,
+		description: "A standard potion that restores 35 health.",
+	},
+	{
+		name: "Major potion",
+		cost: 80,
+		heal: 60,
+		description: "A potent potion that restores 60 health.",
+	},
+	{
+		name: "Full restore",
+		cost: 120,
+		heal: 100,
+		description: "A powerful elixir that restores 100 health.",
+	},
+];
