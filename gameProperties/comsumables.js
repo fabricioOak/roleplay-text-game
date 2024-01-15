fc68ce5b7ab4da56f0525b20da521c4e9d7929a7
Fabricio Carvalho <fcarvalho1999@gmail.com>
2024-01-15 19:13:07 -0300

    update potion values

--- gameProperties/comsumables.js
+++ gameProperties/comsumables.js
@@ -1,25 +1,25 @@
 export default [
 	{
 		name: "Minor potion",
-		cost: 20,
+		cost: 15,
 		heal: 15,
 		description: "A small vial of healing liquid. It restores 15 health.",
 	},
 	{
 		name: "Standard potion",
-		cost: 50,
+		cost: 30,
 		heal: 35,
 		description: "A standard potion that restores 35 health.",
 	},
 	{
 		name: "Major potion",
-		cost: 80,
+		cost: 50,
 		heal: 60,
 		description: "A potent potion that restores 60 health.",
 	},
 	{
 		name: "Full restore",
-		cost: 120,
+		cost: 95,
 		heal: 100,
 		description: "A powerful elixir that restores 100 health.",
 	},
