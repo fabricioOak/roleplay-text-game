026db0da23feea5d0573044d5eac9470ea45b645
Fabricio Carvalho <fcarvalho1999@gmail.com>
2024-01-15 18:02:42 -0300

    change local user var to look for a preset

---
+++ gameProperties/playerInfo.js
@@ -0,0 +1,12 @@
+export default {
+	xp: 0,
+	xpMultiplier: 1,
+	level: 1,
+	xpToNextLevel: 20,
+	health: 100,
+	gold: 10,
+	currentWeapon: 0,
+	fighting: null,
+	monsterHealth: null,
+	inventory: ["Stick"],
+};
