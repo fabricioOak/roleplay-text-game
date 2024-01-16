e46caa5530b9ab9d0aad7c99938ba45191bc9b69
Fabricio Carvalho <fcarvalho1999@gmail.com>
2024-01-16 16:39:25 -0300

    update monsters health

--- gameProperties/monsters.js
+++ gameProperties/monsters.js
@@ -2,7 +2,7 @@ export default [
 	{
 		name: "Slime",
 		level: 2,
-		health: 15,
+		health: 20,
 		description:
 			"A basic and amorphous creature that oozes and splits when attacked.",
 		abilities: ["Split", "Acidic Touch"],
@@ -10,23 +10,39 @@ export default [
 	{
 		name: "Lifeless Creature",
 		level: 2,
-		health: 15,
+		health: 30,
 		description:
 			"A basic and amorphous creature that oozes and splits when attacked.",
 		abilities: ["Split", "Acidic Touch"],
 	},
 	{
+		name: "Shadowling",
+		level: 3,
+		health: 40,
+		description:
+			"A small and elusive creature that hides in the shadows, striking from darkness.",
+		abilities: ["Shadow Step", "Siclent Strike"],
+	},
+	{
 		name: "Fading Shade",
 		level: 4,
-		health: 30,
+		health: 45,
 		description:
 			"A stealthy creature that moves in the shadows, striking when least expected.",
 		abilities: ["Stealth", "Backstab"],
 	},
 	{
+		name: "Ember Sprite",
+		level: 4,
+		health: 55,
+		description:
+			"A mischievous spirit that embodies flames, the Ember Sprite can create small fire illusions.",
+		abilities: ["Fiery Illusion", "Ember Burst"],
+	},
+	{
 		name: "Dead Guardian",
 		level: 4,
-		health: 35,
+		health: 70,
 		description:
 			"An animated stone statue that defends ancient ruins with unyielding strength.",
 		abilities: ["Rock Shield", "Earthquake Slam"],
@@ -34,15 +50,23 @@ export default [
 	{
 		name: "Fangbite Spider",
 		level: 5,
-		health: 40,
+		health: 115,
 		description:
 			"A fiery being that engulfs its enemies in flames, leaving them scorched.",
 		abilities: ["Fire Burst", "Ignite"],
 	},
 	{
+		name: "Stone Golem",
+		level: 5,
+		health: 115,
+		description:
+			"A resilient creature made of solid rock, the Stone Golem can withstand heavy blows.",
+		abilities: ["Rock Shield", "Earth Slam"],
+	},
+	{
 		name: "Stormwing",
 		level: 5,
-		health: 40,
+		health: 115,
 		description:
 			"A winged creature that commands the power of storms, unleashing lightning bolts.",
 		abilities: ["Stormy Flight", "Thunderstrike"],
@@ -50,31 +74,39 @@ export default [
 	{
 		name: "Frostbite",
 		level: 6,
-		health: 50,
+		health: 238,
 		description:
 			"A massive dragon-like creature that freezes its foes with icy breath.",
 		abilities: ["Frost Breath", "Ice Claws"],
 	},
 	{
-		name: "Wraith",
+		name: "Nightmare Steed",
 		level: 6,
-		health: 45,
+		health: 238,
 		description:
-			"A ghostly apparition haunting the depths, draining life force with ethereal attacks.",
-		abilities: ["Ethereal Touch", "Soul Drain"],
+			"A ghostly horse from the underworld, the Nightmare Steed can trample enemies with otherworldly speed.",
+		abilities: ["Spectral Charge", "Ethereal Presence"],
 	},
 	{
 		name: "Thunderclap",
 		level: 7,
-		health: 60,
+		health: 245,
 		description:
 			"A golem charged with electricity, capable of unleashing powerful lightning strikes.",
 		abilities: ["Electric Charge", "Thunder Smash"],
 	},
 	{
+		name: "Giant Spider",
+		level: 7,
+		health: 280,
+		description:
+			"A venomous spider with a paralyzing bite, the Giant Spider can weave webs to trap its prey.",
+		abilities: ["Web Snare", "Venomous Bite"],
+	},
+	{
 		name: "Molten Giant",
 		level: 7,
-		health: 55,
+		health: 280,
 		description:
 			"A colossal creature of molten lava, leaving a trail of destruction in its wake.",
 		abilities: ["Lava Burst", "Magma Slam"],
@@ -82,7 +114,7 @@ export default [
 	{
 		name: "Serpent",
 		level: 8,
-		health: 70,
+		health: 323,
 		description:
 			"A giant serpent with venomous fangs, delivering lethal bites to its prey.",
 		abilities: ["Venom Spit", "Constrict"],
@@ -90,55 +122,15 @@ export default [
 	{
 		name: "Chaos Mage",
 		level: 8,
-		health: 65,
+		health: 323,
 		description:
 			"A mage wielding the chaotic forces of the arcane, warping reality with each spell.",
 		abilities: ["Reality Warp", "Chaos Bolt"],
 	},
 	{
-		name: "Shadowling",
-		level: 3,
-		health: 25,
-		description:
-			"A small and elusive creature that hides in the shadows, striking from darkness.",
-		abilities: ["Shadow Step", "Siclent Strike"],
-	},
-	{
-		name: "Ember Sprite",
-		level: 4,
-		health: 30,
-		description:
-			"A mischievous spirit that embodies flames, the Ember Sprite can create small fire illusions.",
-		abilities: ["Fiery Illusion", "Ember Burst"],
-	},
-	{
-		name: "Stone Golem",
-		level: 5,
-		health: 35,
-		description:
-			"A resilient creature made of solid rock, the Stone Golem can withstand heavy blows.",
-		abilities: ["Rock Shield", "Earth Slam"],
-	},
-	{
-		name: "Nightmare Steed",
-		level: 6,
-		health: 45,
-		description:
-			"A ghostly horse from the underworld, the Nightmare Steed can trample enemies with otherworldly speed.",
-		abilities: ["Spectral Charge", "Ethereal Presence"],
-	},
-	{
-		name: "Giant Spider",
-		level: 7,
-		health: 50,
-		description:
-			"A venomous spider with a paralyzing bite, the Giant Spider can weave webs to trap its prey.",
-		abilities: ["Web Snare", "Venomous Bite"],
-	},
-	{
 		name: "Fanged Beast",
 		level: 8,
-		health: 60,
+		health: 323,
 		description:
 			"A swift and agile creature with sharp fangs, capable of tearing through armor.",
 		abilities: ["Pounce", "Razor Bite"],
@@ -146,7 +138,7 @@ export default [
 	{
 		name: "Swamp Hag",
 		level: 9,
-		health: 55,
+		health: 365,
 		description:
 			"A malevolent witch dwelling in swamps, the Swamp Hag can control nature and curse its foes.",
 		abilities: ["Bog Curse", "Toxic Mist"],
@@ -154,7 +146,7 @@ export default [
 	{
 		name: "Ice Golem",
 		level: 10,
-		health: 100,
+		health: 408,
 		description:
 			"A massive humanoid made of enchanted ice, the Ice Golem can freeze its enemies in their tracks.",
 		abilities: ["Frost Nova", "Icy Slam"],
@@ -162,7 +154,7 @@ export default [
 	{
 		name: "Shapeshifter",
 		level: 11,
-		health: 70,
+		health: 450,
 		description:
 			"A cunning creature capable of assuming the form of its prey, making it difficult to discern friend from foe.",
 		abilities: ["Mimicry", "Deceptive Strike"],
@@ -170,7 +162,7 @@ export default [
 	{
 		name: "Fire Elemental",
 		level: 12,
-		health: 80,
+		health: 493,
 		description:
 			"A living embodiment of flames, the Fire Elemental engulfs its foes in scorching heat. Be cautious, for it can manipulate fire to its advantage.",
 		abilities: ["Inferno Burst", "Flame Shield"],
@@ -178,7 +170,7 @@ export default [
 	{
 		name: "Storm Harpy",
 		level: 13,
-		health: 90,
+		health: 536,
 		description:
 			"A winged creature with the ability to control storms, the Storm Harpy can summon lightning and gusts of wind.",
 		abilities: ["Thunderstorm", "Wind Slash"],
@@ -186,7 +178,7 @@ export default [
 	{
 		name: "Thunder Golem",
 		level: 14,
-		health: 110,
+		health: 578,
 		description:
 			"A golem infused with the power of thunder, capable of unleashing electrifying attacks.",
 		abilities: ["Thunder Strike", "Electric Shield"],
@@ -194,7 +186,7 @@ export default [
 	{
 		name: "Spectral Wraith",
 		level: 15,
-		health: 120,
+		health: 620,
 		description:
 			"An ethereal being that can phase through walls and drain the life force from its victims.",
 		abilities: ["Phantom Phase", "Life Drain"],
@@ -202,7 +194,7 @@ export default [
 	{
 		name: "Earth Elemental",
 		level: 16,
-		health: 130,
+		health: 663,
 		description:
 			"A massive creature made of rock and earth, the Earth Elemental can cause earthquakes and create barriers.",
 		abilities: ["Quake Slam", "Stone Shield"],
@@ -210,7 +202,7 @@ export default [
 	{
 		name: "Mindflayer",
 		level: 17,
-		health: 140,
+		health: 705,
 		description:
 			"An aberrant creature with psychic abilities, the Mindflayer can manipulate minds and devour thoughts.",
 		abilities: ["Mind Blast", "Psychic Drain"],
@@ -218,7 +210,7 @@ export default [
 	{
 		name: "Gorgon Serpent",
 		level: 18,
-		health: 150,
+		health: 748,
 		description:
 			"A serpent with a petrifying gaze, the Gorgon Serpent can turn its victims into stone with a single glare. Approach with extreme caution.",
 		abilities: ["Petrifying Gaze", "Venomous Bite"],
@@ -226,7 +218,7 @@ export default [
 	{
 		name: "Lava Elemental",
 		level: 19,
-		health: 180,
+		health: 1465,
 		description:
 			"A molten creature born from the heart of a volcano, the Lava Elemental can unleash rivers of magma.",
 		abilities: ["Magma Surge", "Fire Immunity"],
@@ -234,7 +226,7 @@ export default [
 	{
 		name: "Lesser Dragon",
 		level: 20,
-		health: 300,
+		health: 2990,
 		description:
 			"A formidable and ancient dragon with powerful breath attacks and thick scales.",
 		abilities: ["Dragon Breath", "Scales of Resistance"],
@@ -242,7 +234,7 @@ export default [
 	{
 		name: "Celestial Guardian",
 		level: 23,
-		health: 380,
+		health: 4490,
 		description:
 			"A divine being sent to protect sacred realms, the Celestial Guardian wields holy powers and celestial weapons.",
 		abilities: ["Divine Smite", "Heavenly Shield"],
@@ -250,7 +242,7 @@ export default [
 	{
 		name: "Demonic Warlord",
 		level: 25,
-		health: 400,
+		health: 5990,
 		description:
 			"A fearsome warlord from the depths of the underworld, wielding dark magic and a massive demonic blade.",
 		abilities: ["Dark Slash", "Infernal Aura"],
@@ -258,17 +250,17 @@ export default [
 	{
 		name: "Shadowsoul Reaper",
 		level: 27,
-		health: 450,
+		health: 7490,
 		description:
-			"A nightmarish entity that harvests souls, the Shadowsoul Reaper can phase between dimensions and unleash dark energy.",
-		abilities: ["Soul Harvest", "Dimensional Shift"],
+			"An ethereal being that can phase through walls and drain the life force from its victims.",
+		abilities: ["Phantom Phase", "Life Drain"],
 	},
 	{
 		name: "Ancient Dragon",
 		level: 30,
-		health: 650,
+		health: 9990,
 		description:
-			"An ancient and powerful dragon with devastating breath attacks and impenetrable scales.",
-		abilities: ["Dragon Breath", "Scales of Resistance", "Dragon Roar"],
+			"An ancient dragon with powerful breath attacks and thick scales.",
+		abilities: ["Dragon Breath", "Scales of Resistance"],
 	},
 ];
